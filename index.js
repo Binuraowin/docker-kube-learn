d6d250714edfe234a720745404f4bfc7accdda48
Binura owin <53115861+Binuraowin@users.noreply.github.com>
2022-12-24 19:39:31 +0530

    added external middlewares :Wq
    :wq
    :WQ
    :wq

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
+
+// Middleware function to log requests
+app.use((req, res, next) => {
+  console.log(`${req.method} request received for ${req.url}`);
+  next();
+});
+
+// Middleware function to parse request body
+app.use(express.json());
+
+// Middleware function to serve static files
+app.use(express.static('public'));
+
+// Middleware function to handle errors
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something went wrong!');
+});
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
